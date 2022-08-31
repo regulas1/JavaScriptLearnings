73d81db087f906da0a6e1bd5bd8ad2fcf6780650
regulas1 <sriregula@outlook.com>
2022-08-31 19:14:54 +1200

    2 files added

---
+++ DataStructures/linkedList.js
@@ -0,0 +1,38 @@
+class Node {
+
+  constructor(data, next = null){
+    this.head = head;
+    this.next = next;
+  
+  }
+//Add
+add(value)
+{
+  const node = new Node(value)
+  let current;
+
+  if(!this.head)
+}
+
+//index of Value
+
+// map
+
+// mapValues
+
+//clear
+
+// from array
+
+// remove from 
+
+
+// get array
+
+// get size
+
+
+
+
+
+}
